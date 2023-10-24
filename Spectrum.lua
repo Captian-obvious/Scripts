59ced89606d23b8b997b30995736d538c8c2cb8f
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-24 11:14:31 -0400

    Create Spectrum.lua

---
+++ Spectrum.lua
@@ -0,0 +1,10 @@
+local plr = script.Player.Value
+function loadMessage()
+    local printerScript = NLS([[
+        print('Audio Spectrum v1.0.0 Loaded!')
+    ]],plr.PlayerGui,false)
+    printerScript.Enabled = true
+    task.wait(1)
+    printerScript:Destroy()
+end
+loadMessage()
