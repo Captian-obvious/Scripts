9144de360bfddd3e9d26ed11fd7a3e8d222e139e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-24 07:24:04 -0400

    Update RelayGame.lua

--- RelayGame.lua
+++ RelayGame.lua
@@ -1,6 +1,12 @@
 local Players=game:GetService('Players')
 local Replicated=game:GetService('ReplicatedStorage')
+local plr = script.Player.Value
 local score=0
+function display(ty,ti,config)
+    local theui = Instance.new('ScreenGui',plr.PlayerGui)
+    theui.Name='Dis_Win_Relay'
+    
+end
 function start()
     score=0
     
