466b60ee6c55cffa1916b1c08e6277e1c5d8f329
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-24 11:24:50 -0400

    Update Spectrum.lua

--- Spectrum.lua
+++ Spectrum.lua
@@ -3,16 +3,18 @@ function loadMessage()
     local printerScript = NLS([[
         print('Audio Spectrum v1.0.0 Loaded!')
     ]],plr.PlayerGui,false)
-    printerScript.Enabled = true
+    printerScript.Enabled=true
     task.wait(1)
     printerScript:Destroy()
 end
 loadMessage()
 --[[ Audio Spectrum Script Created by Fallen and Superduperdev2 --]]
 script.Parent = plr.Character --[[ put the script in the character to avoid it breaking on death--]]
+script.Name='AudioSpectrum_Server'
 local Character = plr.Character
 local Humanoid = (Character and Character.Parent) and Character:FindFirstChildOfClass('Humanoid')
 local sound = Instance.new('Sound',Humanoid.RootPart)
-local analyserNode = require(id)
-local analyser = analyserNode:CreateAnalyser(sound,64)
+local event = Character:FindFirstChild('SpectrumMain') or Instance.new('RemoteEvent',Character)
+event.Name='SpectrumMain'
+
 
