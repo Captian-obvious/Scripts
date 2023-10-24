af948c9786cbc01280ae9bf209cb9a64be869f42
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-24 10:59:22 -0400

    Update and rename worldcmds.lua to worldCmds.lua

--- worldCmds.lua
+++ worldCmds.lua
@@ -5,6 +5,9 @@ function loadMessage()
     local printerScript = NLS([[
         print('InsertWorld Command System Loaded!')
     ]],plr.PlayerGui,false)
+    printerScript.Enabled = true
+    task.wait(1)
+    printerScript:Destroy()
 end
 loadMessage()
 --[[ Basic Commands idk why I made this. --]]
