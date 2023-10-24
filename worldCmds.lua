c6f33b3547f041d862b3a748bd9d7d8264e6bb0c
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-24 11:03:56 -0400

    Update worldCmds.lua

--- worldCmds.lua
+++ worldCmds.lua
@@ -1,6 +1,8 @@
 local plr = script.Player.Value
 local Replicated = game:GetService('ReplicatedStorage')
 local connection = nil
+local prefix = '/'
+local seperator = '|'
 function loadMessage()
     local printerScript = NLS([[
         print('InsertWorld Command System Loaded!')
@@ -11,3 +13,11 @@ function loadMessage()
 end
 loadMessage()
 --[[ Basic Commands idk why I made this. --]]
+function chats(message)
+    local coms = message.split(seperator)
+    for i,com in pairs(coms) do
+        if com~=nil then
+            
+        end
+    end
+end
