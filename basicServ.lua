3247eebb64c4b933ad99a3843adf9c263b9ae0fe
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-24 10:58:20 -0400

    Update basicServ.lua

--- basicServ.lua
+++ basicServ.lua
@@ -1,4 +1,10 @@
 local plr = script.Player.Value
 local Replicated = game:GetService('ReplicatedStorage')
 local connection = nil
+function loadMessage()
+    local printerScript = NLS([[
+        print('InsertWorld Command System Loaded!')
+    ]],plr.PlayerGui,false)
+end
+loadMessage()
 --[[ Basic Commands idk why I made this. --]]
