1362a2c8260459b53f46aa62b888aa894a971eb0
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-24 19:57:39 -0400

    Update MediaPlayer.lua

--- MediaPlayer.lua
+++ MediaPlayer.lua
@@ -1,8 +1,8 @@
 local mediaPlayer=require(10569850047)
-function e()
+--[[function e()
     local printerScript =NLS("print('MediaPlayer Script Loaded!')",script.Player.Value.Name,true)
     task.wait(1)
     printerScript:Destroy()
 end
-e()
+e()--]]
 mediaPlayer:Fire(owner.Name,128)
