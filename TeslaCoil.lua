64fa1c555a239c3a80c6ea863410e19a854f1d25
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-27 08:43:01 -0400

    Update TeslaCoil.lua

--- TeslaCoil.lua
+++ TeslaCoil.lua
@@ -3,13 +3,19 @@ local arc = fromurl('Scripts/Modules/arc.lua')()
 local uiHandler = fromurl('Scripts/Modules/UI/init.lua')()
 local configs = fromurl('Scripts/Modules/TeslaCoil/arcConfig.lua')()
 local arcParams = nil
+local isInitialized = false
 function init()
-    local hat = require(id)
+    local hat = require(id).Hat:Clone()
     hat.Parent = plr.Character
-    arcParams = configs.Initialize(position)
+    local tl = hat:FindFirstChild('topload')
+    if tl then
+        local pos = tl.Position
+        arcParams = configs.Initialize(pos)
+    end
 end
 function pulse(numArcs,pos)
     for i=1,numArcs do
+        local result = Raycast(pos,
         local arcInstance = arc.new(plr.Character,1,{Position1=pos,Position2=pos2,Segments=getSegFromDist()})
     end
 end
