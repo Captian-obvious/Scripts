39e607e8b6f371242424fe86579e6d153bbc2449
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-27 08:38:32 -0400

    Update arc.lua

--- Modules/arc.lua
+++ Modules/arc.lua
@@ -78,82 +78,82 @@ function arc.new(parent,n1,props)
         sfx.Volume = 2
         sfx:Play()
         spawn(function()
-        for c1 = 1, n1 do
-            oldHit = CFrame.new(pos1)
-            for count = 1, math.floor(n2) do
-                local dist = (P.Size.z < 2048) and P.Size.z or arcDist
-                val1 = intToOffset(props.Offset or 1)
-                val2 = dist / n2
-                val3 = P.CFrame.lookVector * -1
-                val4 = count * val2
-                val5 = val4 * val3
-                val6 = pos1
-                Hit = CFrame.new(val5 + val1 + val6)
+            for c1 = 1, n1 do
+                oldHit = CFrame.new(pos1)
+                for count = 1, math.floor(n2) do
+                    local dist = (P.Size.z < 2048) and P.Size.z or arcDist
+                    val1 = intToOffset(props.Offset or 1)
+                    val2 = dist / n2
+                    val3 = P.CFrame.lookVector * -1
+                    val4 = count * val2
+                    val5 = val4 * val3
+                    val6 = pos1
+                    Hit = CFrame.new(val5 + val1 + val6)
+                    local s = Instance.new('Beam')
+                    s.Name = 'ArcPart'..count
+                    s.Parent = arcGroup
+                    local Place0 = oldHit
+                    local m2 = (Place0.p - Hit.p).magnitude
+                    s.Width0=arcWidth
+                    s.Width1=arcWidth
+                    s.FaceCamera = true
+                    s.LightInfluence = 0
+                    s.Brightness = 10
+                    local attach1 = Instance.new('Attachment',gl)
+                    attach1.WorldCFrame = Hit
+                    attach1.Name = 'A1_'..count
+                    local attach2 = Instance.new('Attachment',gl)
+                    attach2.WorldCFrame = Place0
+                    attach2.Name = 'A0_'..count
+                    s.Attachment0=attach2
+                    s.Attachment1=attach1
+                    s.Color = ColorSequence.new(color)
+                    s.Transparency = NumberSequence.new(0)
+                    --[[s.TopSurface = Enum.SurfaceType.Smooth
+                    s.BottomSurface = Enum.SurfaceType.Smooth
+                    s.Size = Vector3.new(arcWidth,arcWidth,m2)
+                    s.CFrame = CFrame.new((Place0.p + Hit.p)/2,Place0.p)
+                    s.Color = color
+                    s.Parent = arcGroup
+                    s.Material = Enum.Material.Neon
+                    s.Anchored = true
+                    s.CanCollide = false
+                    --]]
+                    oldHit = Hit
+                end
+                Hit = oldHit
+                local Place0 = CFrame.new(pos)
                 local s = Instance.new('Beam')
-                s.Name = 'ArcPart'..count
-                s.Parent = arcGroup
-                local Place0 = oldHit
+                s.Name = 'ArcPartEND'
                 local m2 = (Place0.p - Hit.p).magnitude
+                s.Parent = arcGroup
                 s.Width0=arcWidth
                 s.Width1=arcWidth
-                s.FaceCamera = true
                 s.LightInfluence = 0
                 s.Brightness = 10
+                s.FaceCamera = true
                 local attach1 = Instance.new('Attachment',gl)
                 attach1.WorldCFrame = Hit
-                attach1.Name = 'A1_'..count
+                attach1.Name = 'A1_END'
                 local attach2 = Instance.new('Attachment',gl)
                 attach2.WorldCFrame = Place0
-                attach2.Name = 'A0_'..count
+                attach2.Name = 'A0_END'
                 s.Attachment0=attach2
                 s.Attachment1=attach1
                 s.Color = ColorSequence.new(color)
                 s.Transparency = NumberSequence.new(0)
-                --[[s.TopSurface = Enum.SurfaceType.Smooth
-                s.BottomSurface = Enum.SurfaceType.Smooth
-                s.Size = Vector3.new(arcWidth,arcWidth,m2)
-                s.CFrame = CFrame.new((Place0.p + Hit.p)/2,Place0.p)
-                s.Color = color
-                s.Parent = arcGroup
-                s.Material = Enum.Material.Neon
-                s.Anchored = true
-                s.CanCollide = false
-                --]]
-                oldHit = Hit
+                --s.TopSurface = Enum.SurfaceType.Smooth
+                --s.BottomSurface = Enum.SurfaceType.Smooth
+                --s.Size = Vector3.new(arcWidth,arcWidth,m2)
+                --s.CFrame = CFrame.new((Place0.p + Hit.p)/2,Place0.p)
+                --s.Parent = arcGroup
+                --s.Color = color
+                --s.Material = Enum.Material.Neon
+                --s.Anchored = true
+                --s.CanCollide = false
+          
+                wait(0.3)
             end
-            Hit = oldHit
-            local Place0 = CFrame.new(pos)
-            local s = Instance.new('Beam')
-            s.Name = 'ArcPartEND'
-            local m2 = (Place0.p - Hit.p).magnitude
-            s.Parent = arcGroup
-            s.Width0=arcWidth
-            s.Width1=arcWidth
-            s.LightInfluence = 0
-            s.Brightness = 10
-            s.FaceCamera = true
-            local attach1 = Instance.new('Attachment',gl)
-            attach1.WorldCFrame = Hit
-            attach1.Name = 'A1_END'
-            local attach2 = Instance.new('Attachment',gl)
-            attach2.WorldCFrame = Place0
-            attach2.Name = 'A0_END'
-            s.Attachment0=attach2
-            s.Attachment1=attach1
-            s.Color = ColorSequence.new(color)
-            s.Transparency = NumberSequence.new(0)
-            --s.TopSurface = Enum.SurfaceType.Smooth
-            --s.BottomSurface = Enum.SurfaceType.Smooth
-            --s.Size = Vector3.new(arcWidth,arcWidth,m2)
-            --s.CFrame = CFrame.new((Place0.p + Hit.p)/2,Place0.p)
-            --s.Parent = arcGroup
-            --s.Color = color
-            --s.Material = Enum.Material.Neon
-            --s.Anchored = true
-            --s.CanCollide = false
-      
-            wait(0.3)
-        end
         end)
     end
     return arcGroup
