cae3d75479b854aee2b6c4d7f88c5de9ab4a6693
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-27 11:23:58 -0400

    Update io.lua

--- Modules/io.lua
+++ Modules/io.lua
@@ -1,20 +1,32 @@
 local event = script:FindFirstChild('Keypress') or Instance.new('BindableEvent',script)
 event.Name='Keypress'
 local io = {
+    Initialized = false,
     Keypress = event.Event,
 }
 
 function io:Initialize()
-
+    if io.Initialized~=true then
+        io.Initialized = true
+    else
+        warn('IO Mod: Module already initialized!')
+    end
 end
 
-function io:awaitKeypress(key)
-    local plr,pressed = io.Keypress:Wait()
-    if plr~=nil then
-        if pressed==key then
-            return plr,pressed
-        else
-            io:awaitKeypress(key)
+function io:awaitKeypress(mouse,key)
+    if io.Initialized~=true then
+        warn('IO Mod: Module not initialized! Initialize the module before using its functions!')
+    else
+        mouse.KeyDown:Connect(function(k) 
+            event:Fire(owner,k)
+        end)
+        local plr,pressed = io.Keypress:Wait()
+        if plr~=nil then
+            if pressed==key then
+                return plr,pressed
+            else
+                io:awaitKeypress(key)
+            end
         end
     end
 end
