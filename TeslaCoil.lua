28af2731d8610bd28ea6c36e14a9103f9c32a257
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-27 08:18:37 -0400

    Update TeslaCoil.lua

--- TeslaCoil.lua
+++ TeslaCoil.lua
@@ -1,11 +1,16 @@
 local plr = owner
 local arc = fromurl('Scripts/Modules/Arc.lua')()
 local uiHandler = fromurl('Scripts/Modules/UI/init.lua')()
-local arcParams = fromurl('Scripts/Modules/TeslaCoil/arcConfig.lua')()
+local configs = fromurl('Scripts/Modules/TeslaCoil/arcConfig.lua')()
+local arcParams = nil
 function init()
     local hat = require(id)
     hat.Parent = plr.Character
+    arcParams = configs.Initialize(position)
 end
-function pulse()
-    local arcInstance = arc.new(plr.Character,arcParams)
+function pulse(numArcs,pos)
+    for i=1,numArcs do
+        local arcInstance = arc.new(plr.Character,{Position1=pos,Position2=pos2})
+    end
 end
+
