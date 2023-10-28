a7625910b205cabc8a45b125ea0615df268ba7e7
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-27 21:23:01 -0400

    Update TeslaCoil.lua

--- TeslaCoil.lua
+++ TeslaCoil.lua
@@ -27,13 +27,16 @@ function Raycast(pos,direction,ignore)
     local rr = workspace:Raycast(pos,direction,params)
     return rr
 end
+function getSegFromDist(dist)
+    local multiplier
+end
 function pulse(startpos,range,numArcs)
     if isInitialized~=true then
         warn('SSTC: Module Not Initialized! Initialize the module before using its functions!')
     else
         for i=1,numArcs do
-            local dir = Vector3.new(math.random(-180,180),math.random(0,90),math.random(-180,180)).Unit * range
-            local result = Raycast(startpos,dir,{plr.Character})
+            local dir = Vector3.new(math.random(-180,180),math.random(-180,180),math.random(-180,180))
+            local result = Raycast(startpos,dir.Unit * range,{plr.Character})
             local pos = result.Position
             local dist = result.Distance
             local arcInstance = arc.new(plr.Character,1,{ArcColor3=Color3.fromRGB(9, 137, 207),Position1=startpos,Position2=pos,Segments=getSegFromDist(dist),Offset=getOfsFromDist(dist)})
