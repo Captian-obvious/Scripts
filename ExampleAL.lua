d02800a201b043d305f3d6671d3a47e0395e12cf
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-17 23:30:43 -0500

    Update ExampleAL.lua

--- ExampleAL.lua
+++ ExampleAL.lua
@@ -1,4 +1,12 @@
 local vis=Instance.new('Model', workspace)
 vis.Name='Visualizer'
-local al=require (workspace.AudioLib)
+local al=require(workspace.AudioLib)
+if (al.Initialized~=true) then
+    al:Init()
+end
+local actx=al:Create()
+local src=actx:CreateMediaInstanceSource(workspace.Sound)
+local analyser=actx:CreateAnalyser()
+src:Connect(analyser)
+
 
