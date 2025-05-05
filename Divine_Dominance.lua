037886b00349bf75088a3da3afb4684894205202
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-05 10:56:15 -0400

    tool additions

--- Divine_Dominance.lua
+++ Divine_Dominance.lua
@@ -1,5 +1,23 @@
-local Services={};
-function doFinal(plr)
-    local c=plr.Character;
-    local h=c:FindFirstChildOfClass("Humanoid");
+local tool=script.Parent;
+local Services={
+    Players=game:GetService("Players"),
+    ServerStorage=game:GetService("ServerStorage"),
+    TweenService=game:GetService("TweenService"),
+    ReplicatedStorage=game:GetService("ReplicatedStorage")
+};
+function doFinal(plr:Player)
+    if plr.Character then
+        local h:Humanoid=plr.Character:FindFirstChildOfClass("Humanoid");
+    end;
 end;
+function spawnCage(cf:CFrame)
+    local thebox=Instance.new("Model",workspace);
+    thebox.Name="box_of_doom";
+end;
+
+tool.Activated:Connect(function()
+    local plr=Services.Players:GetPlayerFromCharacter(tool.Parent);
+    if plr then
+        -- logic for actually placing a box down that grows and traps the other players
+    end;
+end);
