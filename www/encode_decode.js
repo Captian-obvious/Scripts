632f90c4b61f9ca7cac6ad70ae548ec6a7e2bc97
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-28 20:12:58 -0400

    h

--- www/encode_decode.js
+++ www/encode_decode.js
@@ -1,6 +1,21 @@
 (function(){
     var algs={};
     algs.BASE64=function(ciphertext,mode){
-        return (mode=="DECODE") ? atob(ciphertext) : btoa(ciphertext);
+        return (mode.lower()=="decode") ? atob(ciphertext) : btoa(ciphertext);
     };
+    algs.ASCII=function(ciphertext,mode){
+        return (mode.lower()=="decode") ? atob(ciphertext) : btoa(ciphertext);
+    };
+    var args={args};
+    var action=args[1];
+    var alg=args[2];
+    var ciphertext=args[3];
+    var key=args[4];
+    var returned="";
+    if (alg.lower()=="ascii"){
+        returned=algs.ASCII(ciphertext,action);
+    }else if(alg.lower()=="base64"){
+        returned=algs.BASE64(ciphertext,action);
+    };
+    return returned;
 })();
