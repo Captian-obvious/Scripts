4c64b4a22aeb98ae5db0b1d9ac3dc209ab5ac0df
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-28 20:26:09 -0400

    h2

--- www/encode_decode.js
+++ www/encode_decode.js
@@ -1,19 +1,20 @@
 (function(){
-    var algs={};
-    algs.BASE64=function(ciphertext,mode){
-        return (mode.lower()=="decode") ? atob(ciphertext) : btoa(ciphertext);
-    };
-    algs.ASCII=function(ciphertext,mode){
-        return (mode.lower()=="decode") ? atob(ciphertext) : btoa(ciphertext);
+    var algs={
+        BASE64:function(ciphertext,mode){
+            return (mode.lower()=="decode") ? atob(ciphertext) : btoa(ciphertext);
+        },
+        ASCII:function(ciphertext,mode){
+            return (mode.lower()=="decode") ? atob(ciphertext) : btoa(ciphertext);
+        },
     };
     var action=window.tag_action;
     var alg=window.tag_alg;
     var ciphertext=window.tag_ciphered;
     var key=window.tag_key || "";
     var returned="";
-    if (alg.lower()=="ascii"){
+    if (alg=="ascii"){
         returned=algs.ASCII(ciphertext,action);
-    }else if(alg.lower()=="base64"){
+    }else if(alg=="base64"){
         returned=algs.BASE64(ciphertext,action);
     };
     return returned;
