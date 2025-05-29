5ac347d836f3386043fa1de14b9d72a484f1f22a
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-28 20:36:39 -0400

    tag

--- www/encode_decode.js
+++ www/encode_decode.js
@@ -1,10 +1,16 @@
 (function(){
     var algs={
         BASE64:function(ciphertext,mode){
-            return (mode.lower()=="decode") ? atob(ciphertext) : btoa(ciphertext);
+            return (mode.toLowerCase()=="decode") ? atob(ciphertext) : btoa(ciphertext);
         },
         ASCII:function(ciphertext,mode){
-            return (mode.lower()=="decode") ? atob(ciphertext) : btoa(ciphertext);
+            if (mode == "encode") {
+                return ciphertext.split("").map(char => char.charCodeAt(0)).join(" ");
+            } else if (mode == "decode") {
+                return ciphertext.split(" ").map(code => String.fromCharCode(parseInt(code, 10))).join("");
+            } else {
+                return "Unsupported operation.";
+            };
         },
     };
     var action=window.tag_action;
@@ -12,9 +18,9 @@
     var ciphertext=window.tag_ciphered;
     var key=window.tag_key || "";
     var returned="";
-    if (alg=="ascii"){
+    if (alg.toLowerCase()=="ascii"){
         returned=algs.ASCII(ciphertext,action);
-    }else if(alg=="base64"){
+    }else if(alg.toLowerCase()=="base64"){
         returned=algs.BASE64(ciphertext,action);
     };
     return returned;
