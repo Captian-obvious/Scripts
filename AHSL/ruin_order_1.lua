e0ad39ce936b0bbd891eeccbe4a3ce5fabfe4bc3
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-25 20:32:46 -0400

    h

--- AHSL/ruin_order_1.lua
+++ AHSL/ruin_order_1.lua
@@ -1,6 +1,7 @@
 --!ahsl
 local order={1,3,2,5,4,7,6,8};
 local baseIndex=20;
+local maxDistance=10;
 local buttons={};
 for i=1,#order do
     local res=pcall(function()
@@ -13,12 +14,22 @@ for i=1,#order do
         end;
     end;
 end;
-
+function handleButtonClick(player,button)
+    if not player or not button then return end;
+    local tindex=table.find(buttons,button);
+    if tindex then
+        
+    end;
+end;
 if buttons[1] ~= nil then
     for i=1,#buttons do
         local v=buttons[i];
         if IsA(v,"BasePart") then
             local ClickDetector=Instance.new("ClickDetector",v);
+            ClickDetector.MaxActivationDistance=maxDistance;
+            ClickDetector.MouseClick:Connect(function(player)
+                handleButtonClick(player,v);
+            end);
         end;
     end;
 end;
