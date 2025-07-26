5c4fc0fbaa52f6837c99f19c1bdc51fac6086bf7
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-25 23:11:47 -0400

    h

--- AHSL/ruin_order_1.lua
+++ AHSL/ruin_order_1.lua
@@ -1,56 +1,47 @@
 --!ahsl
 local order={1,3,2,5,4,7,6,8};
-local baseIndex=20;
-local maxDistance=10;
-local orderPart=0; -- the place where we are in the order
 local buttons={};
-local iterator=0;
+local ordered_buttons={};
+local maxDistance=20;
+local orderPart=0;
+local baseName="button";
+function handleButtonClick(player,button)
+	if not player or not button or not button.Parent then return end;
+	if orderPart <= #order then
+		if button==ordered_buttons[orderPart] then
+			print("Order Correct");
+			button.BrickColor=BrickColor.new("Really red");
+			if orderPart==#order then
+				for i,v in pairs(buttons) do
+					v.BrickColor=BrickColor.new("Lime green");
+				end;
+			end;
+		else
+			print("Order Incorrect");
+			orderPart=0;
+			for i,v in pairs(buttons) do
+				v.BrickColor=BrickColor.new("Medium stone grey");
+			end;
+		end;
+	end;
+end;
+--[[ Create the initial table of buttons, and also add click detectors --]]
+for i,v in pairs(workspace.ruins.puzzle1.buttons:GetChildren()) do
+	if v and v.Parent and v:IsA("BasePart") then
+		local tableIndex=tonumber(v.Name:sub(#baseName+1));
+		table.insert(buttons,tableIndex,v);
+		local clickDetector=Instance.new("ClickDetector",v);
+		clickDetector.MaxActivationDistance=maxDistance;
+		clickDetector.MouseClick:Connect(function(player)
+			orderPart+=1; --increment the order step
+			handleButtonClick(player,v);
+		end);
+	end;
+end;
+--[[ Create the ordered buttons list --]]
 for i=1,#order do
-    iterator=iterator+1;
-    local res=pcall(function()
-        table.insert(buttons,iterator,PartIndexing.GetPartsWithIndex(baseIndex+iterator)[1]);
-    end);
-    if not res[1] then
-        warn("No parts with index " ..tostring(baseIndex+order[iterator]).." were found, or an error occured during execution");
-        if res[2] then
-            warn("Error: " .. tostring(res[2]));
-        end;
-    end;
-end;
-function handleButtonClick(player,button,currentButton)
-    if not player or not button then return end;
-    local tindex=table.find(buttons,button);
-    if tindex then
-        local index=order[tindex];
-        print("Button clicked: " .. tostring(button.Name) .. " with index: " ..tostring(index));
-        if button==currentButton then
-            print("Correct order on part "..tostring(orderPart));
-            if orderPart==#order then
-                print("Successful order");
-            end;
-        else
-            print("Unsuccessful Order, reset >:)");
-            orderPart=0;
-        end;
-    end;
-end;
-print(buttons);
-local iterator=0;
-if buttons[1] ~= nil then
-    for i=1,#buttons do
-        iterator=iterator+1;
-        local v=buttons[iterator];
-        if IsA(v,"BasePart") then
-            spawn(function()
-                local ClickDetector=Instance.new("ClickDetector",v);
-                ClickDetector.MaxActivationDistance=maxDistance;
-                ClickDetector.MouseClick:Connect(function(player)
-                    orderPart=orderPart+1;
-                    handleButtonClick(player,v,buttons[orderPart] or nil);
-                end);
-            end);
-        else
-            warn("Button at index " ..tostring(i) .. " is not a BasePart, skipping.");
-        end;
-    end;
+	local button=buttons[order[i]];
+	if button then
+		table.insert(ordered_buttons,button)
+	end;
 end;
