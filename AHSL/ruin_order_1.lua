477960089ac680b3846cd5495abf36b799b11eaf
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-25 20:42:17 -0400

    maybe finish ahsl of ruin_order_1

--- AHSL/ruin_order_1.lua
+++ AHSL/ruin_order_1.lua
@@ -2,6 +2,7 @@
 local order={1,3,2,5,4,7,6,8};
 local baseIndex=20;
 local maxDistance=10;
+local orderPart=0; -- the place where we are in the order
 local buttons={};
 for i=1,#order do
     local res=pcall(function()
@@ -18,7 +19,16 @@ function handleButtonClick(player,button)
     if not player or not button then return end;
     local tindex=table.find(buttons,button);
     if tindex then
-        
+        local index=PartIndexing.GetIndexInPart(button);
+        if index-baseIndex==order[orderPart] then
+            print("Correct order on part "..tostring(index));
+            if orderPart==#order then
+                print("Successful order");
+            end;
+        else
+            print("Unsuccessful Order, reset >:)");
+            orderPart=0;
+        end;
     end;
 end;
 if buttons[1] ~= nil then
@@ -28,6 +38,7 @@ if buttons[1] ~= nil then
             local ClickDetector=Instance.new("ClickDetector",v);
             ClickDetector.MaxActivationDistance=maxDistance;
             ClickDetector.MouseClick:Connect(function(player)
+                orderPart=orderPart+1;
                 handleButtonClick(player,v);
             end);
         end;
