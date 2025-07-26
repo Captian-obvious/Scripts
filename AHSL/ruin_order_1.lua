1ffc2d876f752ffaaf686a4ab4e172db65ca21a2
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-25 20:04:54 -0400

    h

--- AHSL/ruin_order_1.lua
+++ AHSL/ruin_order_1.lua
@@ -3,7 +3,7 @@ local order={1,3,2,5,4,7,6,8};
 local baseIndex=20;
 local buttons={};
 for i=1,#order do
-    local res,err=pcall(function()
+    local res=pcall(function()
         table.insert(buttons,i,PartIndexing.GetPartsWithIndex(baseIndex+order[i])[1]);
     end);
     if not res then
