3e209b19737cbe6a8f40eb5f7e7871bf0de8e3e0
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-25 23:19:34 -0400

    h

--- AHSL/ruin_order_1.lua
+++ AHSL/ruin_order_1.lua
@@ -2,6 +2,7 @@
 local order={1,3,2,5,4,7,6,8};
 local buttons={};
 local ordered_buttons={};
+local raw_buttons=GetChildren(workspace.ruins.puzzle1.buttons)
 local maxDistance=20;
 local orderPart=0;
 local baseName="button";
@@ -12,22 +13,23 @@ function handleButtonClick(player,button)
 			print("Order Correct");
 			button.BrickColor=BrickColor.new("Really red");
 			if orderPart==#order then
-				for i,v in pairs(buttons) do
-					v.BrickColor=BrickColor.new("Lime green");
+				for i=1,#buttons do
+					buttons[i].BrickColor=BrickColor.new("Lime green");
 				end;
 			end;
 		else
 			print("Order Incorrect");
 			orderPart=0;
-			for i,v in pairs(buttons) do
-				v.BrickColor=BrickColor.new("Medium stone grey");
+			for i=1,#buttons do
+				buttons[i].BrickColor=BrickColor.new("Medium stone grey");
 			end;
 		end;
 	end;
 end;
 --[[ Create the initial table of buttons, and also add click detectors --]]
-for i,v in pairs(workspace.ruins.puzzle1.buttons:GetChildren()) do
-	if v and v.Parent and v:IsA("BasePart") then
+for i=1,#raw_buttons do
+    v=raw_buttons[i];
+	if v and v.Parent and IsA(v,"BasePart") then
 		local tableIndex=tonumber(v.Name:sub(#baseName+1));
 		table.insert(buttons,tableIndex,v);
 		local clickDetector=Instance.new("ClickDetector",v);
