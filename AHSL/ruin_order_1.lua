9d1b38e01fd69f6a26425ccb1754e80cd6afdfbe
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-29 09:30:53 -0400

    h

--- AHSL/ruin_order_1.lua
+++ AHSL/ruin_order_1.lua
@@ -7,43 +7,43 @@ local maxDistance=20;
 local orderPart=0;
 local baseName="button";
 function handleButtonClick(player,button)
-	if not player or not button or not button.Parent then return end;
-	if orderPart <= #order then
-		if button==ordered_buttons[orderPart] then
-			print("Order Correct");
-			button.BrickColor=BrickColor.new("Really red");
-			if orderPart==#order then
-				for i=1,#buttons do
-					buttons[i].BrickColor=BrickColor.new("Lime green");
-				end;
-			end;
-		else
-			print("Order Incorrect");
-			orderPart=0;
-			for i=1,#buttons do
-				buttons[i].BrickColor=BrickColor.new("Medium stone grey");
-			end;
-		end;
-	end;
+    if not player or not button or not button.Parent then return end;
+    if orderPart <= #order then
+        if button==ordered_buttons[orderPart] then
+            print("Order Correct");
+            button.BrickColor=BrickColor.new("Really red");
+            if orderPart==#order then
+                for i=1,#buttons do
+                    buttons[i].BrickColor=BrickColor.new("Lime green");
+                end;
+            end;
+        else
+            print("Order Incorrect");
+            orderPart=0;
+            for i=1,#buttons do
+                buttons[i].BrickColor=BrickColor.new("Medium stone grey");
+            end;
+        end;
+    end;
 end;
 --[[ Create the initial table of buttons, and also add click detectors --]]
 for i=1,#raw_buttons do
     v=raw_buttons[i];
 	if v and v.Parent and IsA(v,"BasePart") then
-		local tableIndex=tonumber(v.Name:sub(#baseName+1));
-		table.insert(buttons,tableIndex,v);
-		local clickDetector=Instance.new("ClickDetector",v);
-		clickDetector.MaxActivationDistance=maxDistance;
-		clickDetector.MouseClick:Connect(function(player)
-			orderPart+=1; --increment the order step
-			handleButtonClick(player,v);
-		end);
-	end;
+        local tableIndex=tonumber(v.Name:sub(#baseName+1));
+        table.insert(buttons,tableIndex,v);
+        local clickDetector=Instance.new("ClickDetector",v);
+        clickDetector.MaxActivationDistance=maxDistance;
+        clickDetector.MouseClick:Connect(function(player)
+            orderPart+=1; --increment the order step
+            handleButtonClick(player,v);
+        end);
+    end;
 end;
 --[[ Create the ordered buttons list --]]
 for i=1,#order do
-	local button=buttons[order[i]];
-	if button then
-		table.insert(ordered_buttons,button)
-	end;
+local button=buttons[order[i]];
+if button then
+table.insert(ordered_buttons,button)
+end;
 end;
