9619d8afaf15ec143d9d5856201751fffbe1a840
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-28 23:03:47 -0400

    h

--- www/mainframe_status.js
+++ www/mainframe_status.js
@@ -1,4 +1,16 @@
 (async function(){
-    var thefetch=fetch();
-    
+    let apikey="";
+    var thefetch=await fetch("https://siweb.pythonanywhere.com/app/sieventwebservice/mainframeStatus/get?q=all",{
+        "method":"POST",
+        "headers":{
+            "Content-Type": "application/json",
+            "api-key": apikey,
+        },
+    });
+    try {
+        let thestatus=await thefetch.json();
+    }catch(e){
+        result = `An error occured while fetching status: ${e}`;
+    };
+    return result;
 })();
