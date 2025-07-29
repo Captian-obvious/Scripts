6c19ff71759fb0b834dc3b5efd6260fd90feef01
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-29 19:43:04 -0400

    h

--- www/random_path.js
+++ www/random_path.js
@@ -1,5 +1,3 @@
 (function(){
-    var red="\033[91m";
-    var reset="\033[0m";
-    return `# \`${red}this tag has been disabled${reset}\`\n\n**In light of recent events, this tag has been disabled.**\n\n Im sorry for the inconvenience.\n\nif you have any questions, please contact the site administrator.\n\nthank you for your understanding.\n\n- Superduperdev2 Inc. Webserver team`;
+    return `# \`this tag has been disabled\`\n\n**In light of recent events, this tag has been disabled.**\n\n Im sorry for the inconvenience.\n\nif you have any questions, please contact the site administrator.\n\nthank you for your understanding.\n\n- Superduperdev2 Inc. Webserver team`;
 })()
