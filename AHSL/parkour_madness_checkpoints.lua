dcc5f95d9890830f4a05897d4a3996ef00f4b9df
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-08-11 11:08:22 -0400

    h

--- AHSL/parkour_madness_checkpoints.lua
+++ AHSL/parkour_madness_checkpoints.lua
@@ -2,11 +2,19 @@
 local checkpoints={};
 local playerStatuses={};
 
-function HandleCheckpoint(player, checkpoint)
+function HandleCheckpoint(player, checkpoint:{num:number,part:BasePart})
     if not checkpoints[player] then
         playerStatuses[player]={
             lastCheckpoint=checkpoint,
-            status="active"
+            checkpointsPassed=1,
+            checkpointReached=os.time()
         };
+    else
+        local status=playerStatuses[player];
+        if checkpoint.num > status.lastCheckpoint.num then
+            status.lastCheckpoint=checkpoint;
+            status.checkpointsPassed=status.checkpointsPassed+1;
+            status.checkpointReached=os.time();
+        end;
     end;
 end;
