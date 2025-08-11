dad24e0aefa2b5a7b6c1b78a9a55651567e8b28d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-08-11 13:02:01 -0400

    h

--- AHSL/parkour_madness_handler.lua
+++ AHSL/parkour_madness_handler.lua
@@ -29,8 +29,13 @@ for i=1,#spinParts do
     end);
     task.spawn(function()
         while running do
-            part.CFrame=part.CFrame*CFrame.Angles(0,math.rad(5),0);
-            task.wait(0.1);
+            --AHSL workaround for tweens because TweenService is not available in AHSL
+            local t=Tween.Create(part,TweenInfo.new(3,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut,0,false,0),{CFrame=math.multiply(part.CFrame,CFrame.Angles(0,math.rad(90),0))});
+            t:Play();
+            t.Completed:Wait();
+            if not running then
+                break;
+            end;
         end;
     end);
 end;
