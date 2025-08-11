0c9bb2a33635c1ddd0f2c867b43f644aa78c0227
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-08-11 11:12:14 -0400

    h

--- AHSL/parkour_madness_checkpoints.lua
+++ AHSL/parkour_madness_checkpoints.lua
@@ -1,6 +1,8 @@
 --!ahsl (Uses AHSL library)
 local checkpoints={};
 local playerStatuses={};
+local characterAddedChecks={};
+local baseIndex=130;
 
 function HandleCheckpoint(player, checkpoint:{num:number,part:BasePart})
     if not checkpoints[player] then
@@ -9,12 +11,34 @@ function HandleCheckpoint(player, checkpoint:{num:number,part:BasePart})
             checkpointsPassed=1,
             checkpointReached=os.time()
         };
+        characterAddedChecks[player]=player.CharacterAdded:Connect(function(character)
+            if character and character:IsA("Model") then
+                local humanoid=character:FindFirstChildOfClass("Humanoid");
+                if humanoid then
+                    if playerStatuses[player] then
+                        local theCheckpoint=playerStatuses[player].lastCheckpoint;
+                        if theCheckpoint and theCheckpoint.part then
+                            local part=theCheckpoint.part;
+                            if part:IsA("BasePart") then
+                                local newPosition=part.Position + Vector3.new(0, 5, 0);
+                                character:MoveTo(newPosition);
+                                humanoid.Health=humanoid.MaxHealth;
+                                playerStatuses[player].checkpointReached=os.time();
+                            end;
+                        end;
+                    end;
+                end;
+            end;
+        end);
     else
         local status=playerStatuses[player];
         if checkpoint.num > status.lastCheckpoint.num then
             status.lastCheckpoint=checkpoint;
             status.checkpointsPassed=status.checkpointsPassed+1;
             status.checkpointReached=os.time();
+        else
+            -- Player has returned to a previous checkpoint, do nothing
+            print("Player " .. player.Name .. " returned to a previous checkpoint: " .. checkpoint.num);
         end;
     end;
 end;
