f1c7cc238bfef43591848f9eba3ef0f6e70abd06
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-08-11 23:31:09 -0400

    Update parkour_madness_handler.lua

--- AHSL/parkour_madness_handler.lua
+++ AHSL/parkour_madness_handler.lua
@@ -5,18 +5,18 @@ local spinParts=PartIndexing.GetPartsWithIndex(6);
 for i=1,#killParts do
     local part=killParts[i];
     part.Touched:Connect(function(hit)
-        if hit.Parent and hit.Parent.FindFirstChild("Humanoid") then
-            local humanoid=hit.Parent.Humanoid;
+        if hit.Parent and GetChildOfClass(hit.Parent,"Humanoid") then
+            local humanoid=GetChildOfClass(hit.Parent,"Humanoid");
             humanoid.Health=0;
-        end
+        end;
     end);
 end;
 
 for i=1,#spinParts do
     local part=spinParts[i];
     part.Touched:Connect(function(hit)
-        if hit.Parent and hit.Parent.FindFirstChild("Humanoid") then
-            local humanoid=hit.Parent.Humanoid;
+        if hit.Parent and GetChildOfClass(hit.Parent,"Humanoid") then
+            local humanoid=GetChildOfClass(hit.Parent,"Humanoid");
             humanoid.PlatformStand=true;
             wait(0.5);
             humanoid.PlatformStand=false;
@@ -25,9 +25,8 @@ for i=1,#spinParts do
     task.spawn(function()
         while PartIndexing.GetPartsWithIndex(6)[1] do
             --AHSL workaround for tweens because TweenService is not available in AHSL
-            local t=Tween.Create(part,TweenInfo.new(3,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut,0,false,0),{CFrame=math.multiply(part.CFrame,CFrame.Angles(0,math.rad(90),0))});
-            t:Play();
-            t.Completed:Wait();
+            RunAdonisCommand(":rotatepart 6 relative 0,90,0 5 Linear Out")
+            task.wait(5);
             if not running then
                 break;
             end;
