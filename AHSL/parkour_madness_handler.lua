e92444f384c969b57c904cf2fcfd00fdd6170ea5
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-08-11 23:47:05 -0400

    Update parkour_madness_handler.lua

--- AHSL/parkour_madness_handler.lua
+++ AHSL/parkour_madness_handler.lua
@@ -1,28 +1,5 @@
 --!ahsl
-local killParts=PartIndexing.GetPartsWithIndex(5);
-local spinParts=PartIndexing.GetPartsWithIndex(6);
-
-for i=1,#killParts do
-    local part=killParts[i];
-    part.Touched:Connect(function(hit)
-        if hit.Parent and GetChildOfClass(hit.Parent,"Humanoid") then
-            local humanoid=GetChildOfClass(hit.Parent,"Humanoid");
-            humanoid.Health=0;
-        end;
-    end);
-end;
-
-for i=1,#spinParts do
-    local part=spinParts[i];
-    part.Touched:Connect(function(hit)
-        if hit.Parent and GetChildOfClass(hit.Parent,"Humanoid") then
-            local humanoid=GetChildOfClass(hit.Parent,"Humanoid");
-            humanoid.PlatformStand=true;
-            wait(0.5);
-            humanoid.PlatformStand=false;
-        end;
-    end);
-end;
+RunAdonisCommand(":touchexe 6 started .1 1 :trip exe");
 task.spawn(function()
     while PartIndexing.GetPartsWithIndex(6)[1] do
         RunAdonisCommand(":rotatepart 6 relative 0,90,0 5 Linear Out");
