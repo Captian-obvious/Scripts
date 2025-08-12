ae3e1c2c6f1072fb90b8725b0ee475d0a4fdc809
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-08-11 23:49:32 -0400

    Update parkour_madness_handler.lua

--- AHSL/parkour_madness_handler.lua
+++ AHSL/parkour_madness_handler.lua
@@ -4,7 +4,8 @@ task.spawn(function()
     while PartIndexing.GetPartsWithIndex(6)[1] do
         RunAdonisCommand(":rotatepart 6 relative 0,90,0 5 Linear Out");
         task.wait(5);
-        if not running then
+        if not PartIndexing.GetPartsWithIndex(6)[1] then
+            warn("No parts with index 6 exist, Loop will be terminated");
             break;
         end;
     end;
