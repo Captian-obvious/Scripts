f0956b3e0f56d6bc0d6b6b71cc801ff3ebfed87b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-09-14 18:56:48 -0400

    fetch

--- www/fetch_asset.js
+++ www/fetch_asset.js
@@ -2,9 +2,10 @@ const os=require("os"),fs=require("fs"),path=require("path");
 var platform=os.platform();
 const userAgent=`Superduperdev2IncFetchUtil/1.0.0 {(${os.type()} ${os.release()} [${os.arch()}]) on ${platform}}`;
 const base_url="https://captianobvious.pythonanywhere.com/api/v3/asset";
+var assetid=tag_assetid;
 (async function(){
     try{
-        var url=base_url+"/";
+        var url=base_url+"/"+assetid;
         var response=await fetch(url,{
             "headers":{
                 "User-Agent":userAgent,
@@ -18,6 +19,7 @@ const base_url="https://captianobvious.pythonanywhere.com/api/v3/asset";
             console.log("Request Error: "+message+` (${code})`);
             return "**An error occured while running this command!**\nREQUEST FAILED:\n"+message+` (${code})\n`+` \`\`\`json\n${await response.text()})\n\`\`\``;
         };
+        return `\`\`\`json\n${await response.text()}\`\`\``;
     }catch(error){
         console.log("An error occured while running this command", error);
         return "**An error occured while running this command!**\nTraceback:\n"+error;
