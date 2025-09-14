a2eeb7d1611bce1cbbdc13fc24a5b84b20e4819d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-09-14 18:19:25 -0400

    start

--- www/fetch_asset.js
+++ www/fetch_asset.js
@@ -1,13 +1,15 @@
 const os=require("os"),fs=require("fs"),path=require("path");
 var platform=os.platform();
 const userAgent=`Superduperdev2IncFetchUtil/1.0.0 {(${os.type()} ${os.release()} [${os.arch()}]) on ${platform}}`;
+const url="";
 (async function(){
-    const url="";
     try {
         var response=await fetch(url,{
             "headers":{
                 "User-Agent":userAgent,
+                "Accept":"application/json",
             },
+            "method":"GET",
         });
     }catch(error){
         console.log("An error occured while running this command", error);
