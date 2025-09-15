fa8a2732267ac36d660ac951d094a0fa4b2d5478
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-09-14 20:39:35 -0400

    central return

--- www/fetch_asset.js
+++ www/fetch_asset.js
@@ -3,6 +3,7 @@ const base_url="https://captianobvious.pythonanywhere.com/api/v3/asset";
 var placeid=7081918890;
 var assetid=tag_assetid;
 (async ()=>{
+    var respond="No response returned";
     try{
         var url=`${base_url}/${assetid}?placeId=${placeid}`;
         var response=await fetch(url,{
@@ -17,11 +18,11 @@ var assetid=tag_assetid;
         var returnedJSON=await response.text();
         if (!response.ok){
             console.log("Request Error: "+message+` (${code})`);
-            return "**An error occured while running this command!**\nREQUEST FAILED:\n"+message+` (${code})\n`+` \`\`\`json\n${returnedJSON}\n\`\`\``;
+            respond="**An error occured while running this command!**\nREQUEST FAILED:\n"+message+` (${code})\n`+` \`\`\`json\n${returnedJSON}\n\`\`\``;
         };
-        return "_";
     }catch(error){
         console.log("An error occured while running this command", error);
-        return "**An error occured while running this command!**\nTraceback:\n"+error;
+        respond="**An error occured while running this command!**\nTraceback:\n"+error;
     };
+    return respond;
 })();
