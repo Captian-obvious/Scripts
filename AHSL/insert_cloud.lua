94605343de28b65746cb65bb4a4a4ba75bc5bf3a
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-09-25 21:29:44 -0400

    adonis

--- AHSL/insert_cloud.lua
+++ AHSL/insert_cloud.lua
@@ -1,2 +1,10 @@
 --!ahsl
-local modelJson={};
+local assetId=nil;
+
+RegisterAdonisCommand(":loadasset",1,function(args)
+    assetId=args[1];
+    if tonumber(assetId) then
+        local ID=tonumber(assetId);
+
+    end;
+end);
