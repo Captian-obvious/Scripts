18f75423a92c1df781ed88feaf6c51a86a89302a
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-09-25 21:42:12 -0400

    fetch

--- AHSL/insert_cloud.lua
+++ AHSL/insert_cloud.lua
@@ -5,7 +5,7 @@ local base_uri="https://captianobvious.pythonanywhere.com/api/v3/asset";
 RegisterAdonisCommand(":loadasset",1,function(args)
     assetId=args[1];
     if tonumber(assetId) then
-        local suc,parsed=pcall(function()
+        local ok,parsed=pcall(function()
             local response=Http.Request({
                 Url=base_uri.."/"..assetId.."?placeId="..game.PlaceId, --placeid is required for request to function
                 Method="GET",
@@ -14,7 +14,16 @@ RegisterAdonisCommand(":loadasset",1,function(args)
                 },
             });
             if response.ok then
-                return parsed;
+                print("Response size:",#response.Body);
+                local ok,dat = pcall(function()
+                    return Http.JSONDecode(response.Body);
+                end);
+                if ok then
+                    return dat;
+                else
+                    warn("Failed to parse JSON for asset " .. tostring(assetid) .. ": " .. tostring(dat))
+                    return nil;
+                end;
             else
                 warn("Request to fetch asset failed due to request error: "..response.StatusMessage.."("..tostring(response.StatusCode)..")");
                 return nil;
