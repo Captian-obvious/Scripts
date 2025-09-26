2e6c1bf26c2692978a807b4d56fb822b1fbf28f3
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-09-25 21:37:12 -0400

    api

--- AHSL/insert_cloud.lua
+++ AHSL/insert_cloud.lua
@@ -5,14 +5,17 @@ local base_uri="https://captianobvious.pythonanywhere.com/api/v3/asset";
 RegisterAdonisCommand(":loadasset",1,function(args)
     assetId=args[1];
     if tonumber(assetId) then
-        pcall(function()
-        local response=Http.Request({
-            Url=base_uri..assetId.."?placeId="..game.PlaceId, --placeid is required for request to function
-            Method="GET",
-            Headers={
-                "Accept":"application/json",
-            },
-        })
-        end;
+        local suc,parsed=pcall(function()
+            local response=Http.Request({
+                Url=base_uri.."/"..assetId.."?placeId="..game.PlaceId, --placeid is required for request to function
+                Method="GET",
+                Headers={
+                    "Accept":"application/json",
+                },
+            });
+            if response.ok then
+                return parsed;
+            end;
+        end);
     end;
 end);
