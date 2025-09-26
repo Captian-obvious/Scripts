665d2a916f2ba14d79f347c7e1ba38c8fed0cdab
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-09-25 21:34:26 -0400

    apikey required

--- AHSL/insert_cloud.lua
+++ AHSL/insert_cloud.lua
@@ -1,10 +1,16 @@
 --!ahsl
 local assetId=nil;
+local base_uri="https://captianobvious.pythonanywhere.com/api/v3/asset";
 
 RegisterAdonisCommand(":loadasset",1,function(args)
     assetId=args[1];
     if tonumber(assetId) then
-        local ID=tonumber(assetId);
-
+        local response=Http.Request({
+            Url=base_uri..assetId.."?placeId="..game.PlaceId, --placeid is required for request to function
+            Method="GET",
+            Headers={
+                
+            }
+        })
     end;
 end);
