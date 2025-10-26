78387334bef215f44a272caf7af85d82af57b297
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-10-26 15:24:29 -0400

    h

--- AHSL/insert_cloud.lua
+++ AHSL/insert_cloud.lua
@@ -2,33 +2,35 @@
 local assetId=nil;
 local base_uri="https://captianobvious.pythonanywhere.com/api/v3/asset";
 
+function fetchAndDecode()
+    local response=Http.Request({
+        Url=base_uri.."/"..assetId.."?placeId="..game.PlaceId, --placeid is required for request to function
+        Method="GET",
+        Headers={
+            "Accept":"application/json",
+        },
+    });
+    if response.Success then
+        print("Response size:",#response.Body);
+        local ok,dat = pcall(function()
+            return Http.JSONDecode(response.Body);
+        end);
+        if ok then
+            return dat;
+        else
+            warn("Failed to parse JSON for asset " .. assetId .. ": " .. tostring(dat))
+            return nil;
+        end;
+    else
+        warn("Request to fetch asset failed due to request error: "..response.StatusMessage.." ("..tostring(response.StatusCode)..")");
+        return nil;
+    end;
+end;
+
 RegisterAdonisCommand("loadasset",1,function(args_string)
     assetId=args_string;
     if tonumber(assetId) then
-        local ok,parsed=pcall(function()
-            local response=Http.Request({
-                Url=base_uri.."/"..assetId.."?placeId="..game.PlaceId, --placeid is required for request to function
-                Method="GET",
-                Headers={
-                    "Accept":"application/json",
-                },
-            });
-            if response.Success then
-                print("Response size:",#response.Body);
-                local ok,dat = pcall(function()
-                    return Http.JSONDecode(response.Body);
-                end);
-                if ok then
-                    return dat;
-                else
-                    warn("Failed to parse JSON for asset " .. assetId .. ": " .. tostring(dat))
-                    return nil;
-                end;
-            else
-                warn("Request to fetch asset failed due to request error: "..response.StatusMessage.." ("..tostring(response.StatusCode)..")");
-                return nil;
-            end;
-        end);
+        local ok,parsed=pcall(fetchAndDecode);
         if ok then
             BuildAsset(parsed);
         else
