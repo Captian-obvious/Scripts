233d6797bf814d191ac04c588277fa1d5895e838
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-11-03 17:18:28 -0500

    fmtoobj

--- CPP/fmtoobj.cpp
+++ CPP/fmtoobj.cpp
@@ -4,6 +4,19 @@
 #include <string>
 #include <unordered_map>
 
+struct mesh2Head {
+    ushort sizeof_mesh2Head;
+    byte sizeof_mesh2Vertex;
+    byte sizeof_mesh2Face;
+
+    uint vert_cnt;
+    uint face_cnt;
+}
+
+struct mesh2 {
+    mesh2Head header
+}
+
 int main(int argc,char** argv){
 
 };
