09d13008d82367cd6b65a9e909ec1bcb1bf9e20e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-04-09 13:30:08 -0400

    work on cutscene

--- Lua/Logic/ShadowChase.lua
+++ Lua/Logic/ShadowChase.lua
@@ -4,9 +4,14 @@ local Services={
     Players=game:GetService("Players"),
     TweenService=game:GetService("TweenService")
 };
+local Modules={
+    ArcModule=require(Services.ReplicatedStorage:FindFirstChild("Arc")),
+    CutsceneCameraManager=require(Services.ReplicatedStorage:FindFirstChild("CutsceneCameraManager")),
+    CameraShake=require(Services.ReplicatedStorage:FindFirstChild("CameraShakeNew"))
+};
 
 function spawnEntity(entityName:string,cf:CFrame)
-    local entityFolder=Services.ReplicatedStorage:FindFirstChild("Entities");
+    local entityFolder=Services.ReplicatedStorage:FindFirstChild("Entities") or Services.ServerStorage:FindFirstChild("Entities");
     if entityFolder then
         local entity=entityFolder:FindFirstChild(entityName);
         if not entity then return end;
