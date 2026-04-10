f4dabdc213a3bbf154442227fa5057d8f2b1e6d9
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-04-10 14:09:54 -0400

    start working on chat bubble handler

---
+++ Lua/Logic/ChatBubbleHandler.lua
@@ -0,0 +1,11 @@
+local mod={};
+local Service={
+    Players=game:GetService("Players"),
+    TextChatService=game:GetService("TextChatService"),
+    ReplicatedStorage=game:GetService("ReplicatedStorage")
+};
+
+function mod.init()
+end;
+
+return mod;
