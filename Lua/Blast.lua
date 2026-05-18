408306e69ccf8577a850e42bc75309ee165526e3
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-05-18 17:56:42 -0400

    h

--- Lua/Blast.lua
+++ Lua/Blast.lua
@@ -17,6 +17,8 @@ local kaboom = sf:FindFirstChild('kaboom') or makeSound('rbxassetid://131026234'
 local countdown=sf:FindFirstChild('tentozero') or makeSound("rbxassetid://15487574881",3,false,'tentozero');
 local sdm1=sf:FindFirstChild('t6min') or makeSound("rbxassetid://1000895732",2,false,'t6min');
 local music1=sf:FindFirstChild('music1') or makeSound("rbxassetid://9254549777",2,false,'music1');
+local music2=sf:FindFirstChild('music2') or makeSound("rbxassetid://9254549777",2,false,'music2');
+local music3=sf:FindFirstChild('music3') or makeSound("rbxassetid://9254549777",2,false,'music3');
 while not music1.IsLoaded do
   task.wait()
 end;
